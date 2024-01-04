554ab83266568c4bbfd19da9238a55f42f9002f0
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2024-01-04 18:48:46 +0530

    Changed Excel Sheet format

--- src/main/java/org/example/ExcelSheet.java
+++ src/main/java/org/example/ExcelSheet.java
@@ -19,22 +19,27 @@ public class ExcelSheet {
     public static void generateDataAndCharts(Sheet combinedSheet) {
         Row headingRow = combinedSheet.createRow(0);
         headingRow.createCell(0).setCellValue("Sr. No");
-        headingRow.createCell(1).setCellValue("Question Type (Audio / Video / Image)");
+        headingRow.createCell(1).setCellValue("Question Type");
         headingRow.createCell(2).setCellValue("Answer Type");
         headingRow.createCell(3).setCellValue("Topic Number");
-        headingRow.createCell(4).setCellValue("Questions");
+        headingRow.createCell(4).setCellValue("Question (Text Only)");
         headingRow.createCell(5).setCellValue("Correct Answer 1");
-        headingRow.createCell(6).setCellValue("Correct 2");
-        headingRow.createCell(7).setCellValue("Correct 3");
-        headingRow.createCell(8).setCellValue("Correct 4");
+        headingRow.createCell(6).setCellValue("Correct Answer 2");
+        headingRow.createCell(7).setCellValue("Correct Answer 3");
+        headingRow.createCell(8).setCellValue("Correct Answer 4");
         headingRow.createCell(9).setCellValue("Wrong Answer 1");
         headingRow.createCell(10).setCellValue("Wrong Answer 2");
         headingRow.createCell(11).setCellValue("Wrong Answer 3");
-        headingRow.createCell(12).setCellValue("Time in Seconds");
+        headingRow.createCell(12).setCellValue("Time in seconds");
         headingRow.createCell(13).setCellValue("Difficulty Level");
         headingRow.createCell(14).setCellValue("Question (Audio / Video / Image)");
         headingRow.createCell(15).setCellValue("Contributor's Registered mailId");
-        headingRow.createCell(16).setCellValue("Variation Number");
+        headingRow.createCell(16).setCellValue("Solution (Text Only)");
+        headingRow.createCell(17).setCellValue("Solution (Image/ Audio/ Video)");
+        headingRow.createCell(18).setCellValue("Variation Number");
+        headingRow.createCell(19).setCellValue("");
+
+
 
         for (int i = 0; i < 200; i++) {
             String chartTitle = "Chart " + (i + 1);
@@ -50,52 +55,61 @@ public class ExcelSheet {
 
             saveChartAsImage(barChart, imagePath);
 
-            Row dataRow = combinedSheet.createRow(i + 1);
-            dataRow.createCell(0).setCellValue(i + 1);
-            dataRow.createCell(1).setCellValue("Image");
-            dataRow.createCell(2).setCellValue(1);
-            dataRow.createCell(3).setCellValue("09030201");
-
-            String[] questions = Arrays.copyOfRange(Questions.getRandomQuestions(13), 0, 5);
-            String[] answers = new String[5];
             for (int j = 0; j < 5; j++) {
-                answers[j] = Answers.getAnswer(questions[j], values , Arrays.stream(categories).toList());
+                Row dataRow = combinedSheet.createRow((i * 5) + j + 1);
+                dataRow.createCell(0).setCellValue((i * 5) + j + 1);
+                dataRow.createCell(1).setCellValue("Image");
+                dataRow.createCell(2).setCellValue(1);
+                dataRow.createCell(3).setCellValue("09030201");
+
+                String[] questions = Arrays.copyOfRange(Questions.getRandomQuestions(13), 0, 1); // Get only 1 question
+                String[] answers = new String[5];
+
+                answers[0] = Answers.getAnswer(questions[0], values, Arrays.stream(categories).toList());
+                dataRow.createCell(4).setCellValue(questions[0]);
+                dataRow.createCell(5).setCellValue(answers[0]);
+
+                dataRow.createCell(6).setCellValue("");
+                dataRow.createCell(7).setCellValue("");
+                dataRow.createCell(8).setCellValue("");
+
+                String wrongAns1 = "";
+                String wrongAns2 = "";
+                String wrongAns3 = "";
+
+                String[] listOfWrongAnswers = WrongAnswers.generateWrongAnswers(answers[0]);
+                wrongAns1 = listOfWrongAnswers[0];
+                wrongAns2 = listOfWrongAnswers[1];
+                wrongAns3 = listOfWrongAnswers[2];
+
+                dataRow.createCell(9).setCellValue(wrongAns1);
+                dataRow.createCell(10).setCellValue(wrongAns2);
+                dataRow.createCell(11).setCellValue(wrongAns3);
+
+                dataRow.createCell(12).setCellValue("60");
+                dataRow.createCell(13).setCellValue(4);
+                if(j == 0){
+                    dataRow.createCell(14).setCellValue(imagePath);
+                }
+
+
+                dataRow.createCell(15).setCellValue("2022.chinmay.chaudhari@ves.ac.in");
+
+                dataRow.createCell(18).setCellValue(110);
+
+                if( j % 5 == 0){
+                    dataRow.createCell(19).setCellValue(1);
+                }else{
+                    dataRow.createCell(19).setCellValue(2);
+                }
             }
-            dataRow.createCell(4).setCellValue(String.join("\n", questions));
-            dataRow.createCell(5).setCellValue(String.join("\n", answers));
 
-            dataRow.createCell(6).setCellValue("");
-            dataRow.createCell(7).setCellValue("");
-            dataRow.createCell(8).setCellValue("");
-
-
-            String wrongAns1 = "";
-            String wrongAns2 = "";
-            String wrongAns3 = "";
-            for (int j = 0; j < 5; j++) {
-                String[] listOfWrongAnswers =  WrongAnswers.generateWrongAnswers(answers[j]);
-                wrongAns1 = wrongAns1 + listOfWrongAnswers[0] + "\n";
-                wrongAns2 = wrongAns2 + listOfWrongAnswers[1] + "\n";
-                wrongAns3 = wrongAns3 + listOfWrongAnswers[2] + "\n";
-            }
-            dataRow.createCell(9).setCellValue(wrongAns1);
-            dataRow.createCell(10).setCellValue(wrongAns2);
-            dataRow.createCell(11).setCellValue(wrongAns3);
-
-
-            dataRow.createCell(12).setCellValue("60s");
-            dataRow.createCell(13).setCellValue("Easy");
-
-            dataRow.createCell(14).setCellValue(imagePath);
-
-            dataRow.createCell(15).setCellValue("2022.chinmay.chaudhari@ves.ac.in");
-
-            dataRow.createCell(16).setCellValue(110);
         }
-
-        for (int i = 0; i < 27; i++) {
+        for (int i = 0; i < 16; i++) {
             combinedSheet.autoSizeColumn(i);
         }
+        Row lastRow = combinedSheet.createRow(1001);
+        lastRow.createCell(0).setCellValue("****");
     }
 
     private static String[] generateRandomCategories() {
@@ -122,10 +136,5 @@ public class ExcelSheet {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static String getTopicNumber(int index) {
-        String[] topicNumbers = {"09", "03", "02", "01"};
-        return topicNumbers[index % topicNumbers.length];
     }
 }
